7f877026c32c8a58832ddfe6b091b92b9740ba05
opeyemipeter1759 <opeyemiadebowale1759@gmail.com>
2023-03-12 00:07:13 +0100

    dashboard update

--- src/pages/Dashboard/Dashboard.js
+++ src/pages/Dashboard/Dashboard.js
@@ -1,11 +1,20 @@
 import { Box } from "@mui/material"
 import Header from '../../components/Header';
 
-const Dashboard = () => {
+const Dashboard = () =>
+{
+    
+    let myDate = new Date();
+    let hrs = myDate.getHours();
+    let greet;
+    if (hrs < 12) greet = " Morning";
+    else if (hrs >= 12 && hrs <= 17) greet = " Afternoon";
+    else if (hrs >= 17 && hrs <= 24) greet = " Evening";
   return (
     <Box m='20px'>
       <Box display="flex" justifyContent="space-between" alignItems="center">
-        <Header title='Dashboard' subtitle='welcome to your dashboard' />
+              <Header title='Dashboard' subtitle={`${greet} Alex, Welcome to your dashboard   `} />
+            
       </Box>
     </Box>
   );
