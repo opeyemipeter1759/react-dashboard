45c4b134c52131f113b5e377305893a8761d455c
opeyemipeter1759 <opeyemiadebowale1759@gmail.com>
2023-03-14 08:05:32 +0100

    fix Calendar

--- src/pages/Form/Form.js
+++ src/pages/Form/Form.js
@@ -122,7 +122,7 @@ const Form = () => {
                 sx={{ gridColumn: 'span 4' }}
               />
             </Box>
-            <Box display='flex' justifyContent='end' mt='20px'>
+            <Box display='flex' justifyContent='start' mt='20px'>
               <Button type='submit' color='secondary' variant='contained'>
                 Create New User
               </Button>
